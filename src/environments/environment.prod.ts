f28e465869f95a69a3f4a139521324c268ec6297
Danodya Weerasinghe <danoweera95@gmail.com>
2017-12-19 19:19:51 +0530

    First commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDgsQGCJB0w7tNpjye8rQd-WY6hZeZJVAA',
+    authDomain: 'base-chat-dano.firebaseapp.com',
+    databaseURL: 'https://base-chat-dano.firebaseio.com',
+    projectId: 'base-chat-dano',
+    storageBucket: 'base-chat-dano.appspot.com',
+    messagingSenderId: '683827470159'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDgsQGCJB0w7tNpjye8rQd-WY6hZeZJVAA',
+    authDomain: 'base-chat-dano.firebaseapp.com',
+    databaseURL: 'https://base-chat-dano.firebaseio.com',
+    projectId: 'base-chat-dano',
+    storageBucket: 'base-chat-dano.appspot.com',
+    messagingSenderId: '683827470159'
+  }
 };
